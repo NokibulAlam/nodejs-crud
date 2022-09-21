943913b57db75b0c762a008b1058a7adb6fb7104
NokibulAlam <nokibul.alam02@gmail.com>
2022-09-21 22:51:00 +0600

    connect-mongodb-session ADDED and implemented

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const userRouter = require('./routes/users');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
+const store = new MongoDBStore({
+    uri: "mongodb://localhost:27017/testNode",
+    collection: "session",
+})
+
 app.use(session({
     secret: "secret",
     resave: false,
     saveUninitialized: false,
+    store: store,
 }));
 
 app.set("view engine", "ejs");
